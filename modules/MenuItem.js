0087816ddd68b0bd979bf4ae161f5947f2959bde
Leon Robert <ler@rmsiasi.onmicrosoft.com>
2020-01-08 16:08:15 +0200

    final change

--- modules/MenuItem.js
+++ modules/MenuItem.js
@@ -3,65 +3,47 @@ function MenuItem (componentsArray){
         return new MenuItem(componentsArray);
     }
     this.componentsArray = componentsArray;
-    this.domReference = null;
-    this.lastLabel = null;
-    this.menulevel = 0;
 }
 
 Object.assign(MenuItem.prototype, {
     init: function(node, parentConfig, component = null) {
       //iterate and construct models
-      console.log(component)
-      this.lastLabel = node;
       let componentsArray = this.componentsArray;
       if(component === null){
           component = componentsArray[parentConfig];
       }
-      for(let i= 0; i<component.length ; i++ ){
-            console.log(i);
-            this.nodeDecider(this.lastLabel, component[i], i);
-      }
+      console.log(component + "component")
+      console.log(component.length);
+      this.goThroughArray(node, component)
     },
-
-    nodeDecider: function(node, object, level){
-        console.log(object.label+ "  -label-  ");
-        console.log(node + "  -label-node-  ");
-        let label = this.render(node, object.label);
-        if((this.menulevel !== 0 && level!==0) || (this.menulevel === 0 && level!==0) || (this.menulevel !== 0 && level===0)){
-            console.log("MenuLevel"+ this.menulevel+"  level"+level)
-            this.lastLabel = label;
+    goThroughArray: function(node,component){
+        for(let i= 0; i<component.length ; i++ ){
+            this.nodeDecider(node, component[i]);
         }
+
+    },
+    nodeDecider: function(node, object){
+        let element = this.render(node, object.label);
         if(object.submenu !== undefined){
-            this.init(this.lastLabel, "submenu", object.submenu);
+            let wrapper = this.createParentNode(element, object.label);
+            this.goThroughArray(wrapper, object.submenu);
         }
     },
-
+    createParentNode: function(wrapper, label){
+        let domElement = document.createElement("ul");
+        let depth = (label.toString().replace(/ /g,'').replace(/\./g,"_").toLowerCase().match(new RegExp("_", "g"))|| []).length;
+        domElement.classList.add("level_"+(depth+1)+"_menu");
+        $(wrapper).append(domElement);
+        return domElement;
+    },
     render: function(wrapper, label) {
         let domElement;
-        let depth = (label.toString().replace(/ /g,'').replace(/\./g,"_").toLowerCase().match(new RegExp("_", "g"))|| []).length;
-        if(depth !== this.menulevel){
-            this.menulevel = depth;
-            domElement = document.createElement("ul");
-            domElement.classList.add("level_"+depth+"_menu");
-            
-
-            let li = document.createElement("li");
-            
-            let name = document.createElement("a");
-            let textchild = document.createTextNode(label);
-            name.appendChild(textchild);
-            li.appendChild(name);
-            domElement.appendChild(li);
-        }
-        else{
-            domElement = document.createElement("li");
-            let name = document.createElement("a");
-            let textchild = document.createTextNode(label);
-            name.appendChild(textchild);
-            domElement.appendChild(name);
-        }
-
-        this.domReference = domElement;
+        domElement = document.createElement("li");
+        let name = document.createElement("a");
+        let textchild = document.createTextNode(label);
+        name.appendChild(textchild);
+        domElement.appendChild(name);
+        
         $(wrapper).append(domElement);
         return domElement;
     }
